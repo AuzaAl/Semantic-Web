213f80ab86511d3a51eeccbcc4dcc49b92a5c7c1
AuzaAl <auzaganteng007@gmail.com>
2025-05-06 04:53:12 +0700

    add several files

---
+++ assets/script/script.js
@@ -0,0 +1,7 @@
+document.getElementById("hamburger").onclick = function() {
+    document.getElementById("sidebar").style.width = "270px";
+}
+
+document.getElementById("closebtn").onclick = function() {
+    document.getElementById("sidebar").style.width = "0";
+}
